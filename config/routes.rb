7463b441aa42c0282cc1cbb78520b0eff47b1636
Aurelia Nowak <nowakaurelia@gmail.com>
2020-02-18 16:48:38 +0100

    Models & CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ # As a user, I can list tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # As a user, I can add a new task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :task_new
+  # As a user, I can edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+    # As a user, I can view the details of a task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+
+  patch 'tasks/:id', to: 'tasks#update'
+  # As a user, I can remove a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
